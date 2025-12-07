f0647832502a5710727f99cc08eb5d5a946336c5
Niall Smart <niall.smart@gmail.com>
2025-12-07 14:52:45 -0500

    solution to day3

--- day3/day3.py
+++ day3/day3.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
-# example.txt: 1227775554
-# input.txt: 37314786486
+# example.txt: 357
+# input.txt: 17405
 
 import sys
 
-def process(lines):
-    return None
+def process(banks):
+    total = 0
+    for bank in banks:
+        batteries = [int(b) for b in bank]
+        bankmax = 0
+        for i, bi in enumerate(batteries[:-1]):
+            bi *= 10
+            if bi > bankmax // 10:
+                mbj = max(batteries[i + 1:])
+                bankmax = max(bi + mbj, bankmax)
+        total += bankmax
+    return total
+
 
 def main():
     if len(sys.argv) < 3:
