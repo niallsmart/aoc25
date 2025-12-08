22d8de60dfbf6c26b7e76b919e9910dfd1ea504a
Niall Smart <niall.smart@gmail.com>
2025-12-07 22:14:02 -0500

    solve day1 part2 (two versions)

--- day1/day1.py
+++ day1/day1.py
@@ -1,27 +1,80 @@
 #!/usr/bin/env python3
 
-# input.txt: 1152
-# example.txt: 3
+# input.txt: 1152 / 6671
+# example.txt: 3 / 6
 
 import sys
 import operator
+import math
 from itertools import accumulate
 
-def process_loop(lines):
-    dial = 50
-    password = 0
-    rotations = [(l[0], int(l[1:])) for l in lines]
 
+def process_part2_v1(lines):
+
+    def turn_right(start, distance, collect = []):
+        move = min(distance, 100 - start)
+        start += move
+        start %= 100
+        distance -= move
+        collect.append((start, move))
+        if distance > 0:
+            turn_right(start, distance, collect)
+
+    def turn_left(start, distance, collect = []):
+        if start == 0:
+            move = min(distance, 100)
+        else:
+            move = min(distance, start)
+        start -= move
+        start %= 100
+        distance -= move
+        collect.append((start, move))
+        if distance > 0:
+            turn_left(start, distance, collect)
+
+
+    rotations = [(l[0], int(l[1:])) for l in lines]        
+    position = 50
+    password = 0
+    
     for (dir, distance) in rotations:
-        if dir == "L":
-            distance *= -1
-        dial += distance
-        if dial % 100 == 0:
-            password += 1
+        turns = []
+        if dir == 'L':
+            turn_left(position, distance, turns)
+        else:
+            turn_right(position, distance, turns)
+
+        password += sum(1 for s, _ in turns if s == 0)
+        position = turns[-1][0]
 
     return password
 
-def process_func(lines):
+
+def process_part2_v2(lines):
+    rotations = [(l[0], int(l[1:])) for l in lines]        
+    position = 50
+    password = 0
+    
+    for (dir, distance) in rotations:
+
+        password += distance // 100
+        rem = distance % 100
+
+        if dir == 'L':
+            position -= rem
+            if position <= 0 and (position + rem) > 0:
+                password += 1 
+        else:
+            position += rem
+            if position >= 100:
+                password += 1 
+
+        position %= 100
+
+
+    return password
+
+def process_part1(lines):
     sign = {"L": -1, "R": +1}
     rotations = [int(l[1:]) * sign[l[0]] for l in lines]        
     dials = accumulate(rotations, operator.add, initial=50)
@@ -35,10 +88,11 @@ def main():
 
     with open(sys.argv[1]) as f:
         expected = int(sys.argv[2])
-        actual = process_func(f.read().splitlines())
+        actual = process_part2_v2(f.read().splitlines())
 
         assert actual == expected, f"Expected {expected}, got {actual}"
         print(f"Success: {expected}")
 
 if __name__ == "__main__":
     main()
+
